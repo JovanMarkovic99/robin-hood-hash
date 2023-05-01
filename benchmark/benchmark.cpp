b92f29faa9f96514cac524ec2073f1852c3ffc37
Jovan Markovic <joca.dasa@yahoo.com>
2023-05-01 18:25:37 +0200

    Fixed styling inconsistencies and noted user defined areas

--- benchmark/benchmark.cpp
+++ benchmark/benchmark.cpp
@@ -10,30 +10,42 @@
 
 #include "../map.h"
 
+
+// USER DEFINED --------------------------------------------------------------------------
+
 #define NUM_ITERATIONS 100
 
 using KeyType = int;
 using ValueType = int;
-using KeyValueType = std::pair<KeyType, ValueType>;
 using MapType = jvn::unordered_map<KeyType, ValueType>;
 
+KeyType getKey(std::string input) {   
+    return std::stoi(input);
+}
+
+ValueType getValue(std::string input) {
+    return std::stoi(input);
+}
+
+
+// END USER DEFINED ----------------------------------------------------------------------
+
+
+using KeyValueType = std::pair<KeyType, ValueType>;
 using Clock = std::chrono::high_resolution_clock;
 
 
-void printData(const std::vector<KeyValueType>& data_vec, uint64_t avrg, uint64_t stddev, std::string function_name)
-{
+void printData(const std::vector<KeyValueType>& data_vec, uint64_t avrg, uint64_t stddev, std::string function_name) {
     std::cout << "Finished benchmarking " << function_name << " after " << NUM_ITERATIONS << " iterations.\n";
     std::cout << "Average total time: " << avrg / 1000.0L << "ms +/- " << stddev / 1000.0L << "ms\n";
     std::cout << "Average per-element time: " << avrg / data_vec.size() << "ns\n\n";
 }
 
-std::pair<uint64_t, uint64_t> calcStats(const std::array<uint64_t, NUM_ITERATIONS>& measurements)
-{
+std::pair<uint64_t, uint64_t> calcStats(const std::array<uint64_t, NUM_ITERATIONS>& measurements) {
     // Overflow-free average calculation
     uint64_t avrg = 0,
         reminder = 0;
-    for (uint64_t time: measurements)
-    {
+    for (uint64_t time: measurements) {
         avrg += time / NUM_ITERATIONS;
         reminder += time % NUM_ITERATIONS;
     }
@@ -46,8 +58,7 @@ std::pair<uint64_t, uint64_t> calcStats(const std::array<uint64_t, NUM_ITERATION
     return {avrg, sqrt(sum_of_squared_diff / NUM_ITERATIONS)};
 }
 
-uint64_t measureErase(const std::vector<KeyValueType>& data_vec)
-{
+uint64_t measureErase(const std::vector<KeyValueType>& data_vec) {
     MapType map;
     for (auto key_value: data_vec)
         map.insert(key_value);
@@ -60,15 +71,13 @@ uint64_t measureErase(const std::vector<KeyValueType>& data_vec)
     return std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start).count();
 }
 
-uint64_t measureFind(const std::vector<KeyValueType>& data_vec)
-{
+uint64_t measureFind(const std::vector<KeyValueType>& data_vec) {
     MapType map;
     for (auto key_value: data_vec)
         map.insert(key_value);
 
     auto start = Clock::now();
-    for (auto [key, value]: data_vec)
-    {
+    for (auto [key, value]: data_vec) {
         auto iter = map.find(key);
         if (iter == map.end())
             std::cout << "ERROR! Key not found in map!\n";
@@ -78,8 +87,7 @@ uint64_t measureFind(const std::vector<KeyValueType>& data_vec)
     return std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start).count();
 }
 
-uint64_t measureInsertion(const std::vector<KeyValueType>& data_vec)
-{
+uint64_t measureInsertion(const std::vector<KeyValueType>& data_vec) {
     MapType map;
 
     auto start = Clock::now();
@@ -90,14 +98,14 @@ uint64_t measureInsertion(const std::vector<KeyValueType>& data_vec)
     return std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start).count();
 }
 
-std::pair<uint64_t, uint64_t> measure(const std::vector<KeyValueType>& data_vec, std::function<uint64_t(const std::vector<KeyValueType>&)> measuring_func, std::string function_name)
-{
+std::pair<uint64_t, uint64_t> measure(const std::vector<KeyValueType>& data_vec, 
+                                    std::function<uint64_t(const std::vector<KeyValueType>&)> measuring_func, 
+                                    std::string function_name) {
     std::cout << "Benchmarking " << function_name << "...\n";
 
     std::array<uint64_t, NUM_ITERATIONS> measurements;
     std::cout << "Iteration ";
-    for (int i = 0; i < NUM_ITERATIONS; ++i)
-    {
+    for (int i = 0; i < NUM_ITERATIONS; ++i) {
         std::cout << i + 1;
 
         measurements[i] = measuring_func(data_vec);
@@ -110,8 +118,7 @@ std::pair<uint64_t, uint64_t> measure(const std::vector<KeyValueType>& data_vec,
     return calcStats(measurements);
 }
 
-void runBenchmark(const std::vector<KeyValueType>& data_vec)
-{
+void runBenchmark(const std::vector<KeyValueType>& data_vec) {
     auto [avrg_insertion, dev_insertion] =  measure(data_vec, measureInsertion, "insertions");
     printData(data_vec, avrg_insertion, dev_insertion, "insertions");
 
@@ -124,39 +131,33 @@ void runBenchmark(const std::vector<KeyValueType>& data_vec)
 
 
 int main(int argc, char* argv[]) {
-    if (argc < 2) 
-    {
+    if (argc < 2) {
         std::cerr << "Usage: " << argv[0] << " <filename>\n";
         return 1;
     }
 
     std::ifstream input_file(argv[1]);
-    if (!input_file.is_open())
-    {
+    if (!input_file.is_open()) {
         std::cerr << "Could not open file: " << argv[1] << "\n";
         return 1;
     }
 
     std::vector<KeyValueType> data_vec;
     std::string line;
-    while (getline(input_file, line))
-    {
+    while (getline(input_file, line)) {
         auto comma_pos = line.find(',');
-        if (comma_pos == std::string::npos)
-        {
+        if (comma_pos == std::string::npos) {
             std::cerr << "Error: invalid CSV line: " << line << '\n';
             continue;
         }
 
-        try 
-        {
-            KeyType key = std::stoi(line.substr(0, comma_pos));
-            ValueType value = std::stoi(line.substr(comma_pos + 1));
+        try {
+            KeyType key = getKey(line.substr(0, comma_pos));
+            ValueType value = getKey(line.substr(comma_pos + 1));
 
             data_vec.emplace_back(key, value);
         }
-        catch (std::exception e)
-        {
+        catch (std::exception e) {
             std::cerr << "Error while parsing CSV line: " << line << '\n';
         }
     }
